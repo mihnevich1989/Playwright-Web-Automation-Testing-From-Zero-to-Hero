a563970a00ac896840f9b77dbc5c98a9d1f977b2
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-25 23:01:25 +0300

    27. Parent Elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -37,10 +37,16 @@ test('Locator syntax rules', async ({ page }) => {
 
 test('User facing locators', async ({ page }) => {
 
-  await page.getByRole('textbox', { name: "Email" }).first().click()
-  await page.getByRole('button', { name: "sign in" }).first().click()
+  await page.getByRole('textbox', { name: "Email" })
+    .first()
+    .click()
+  await page.getByRole('button', { name: "sign in" })
+    .first()
+    .click()
 
-  await page.getByLabel('Email').first().click()
+  await page.getByLabel('Email')
+    .first()
+    .click()
 
   await page.getByPlaceholder('Jane Doe').click()
 
@@ -55,9 +61,48 @@ test('locating child elements', async ({ page }) => {
   await page.locator('nb-card nb-radio :text-is("Option 1")').click()
   await page.locator('nb-card nb-radio :text-is("Option 2")').check()
 
-  await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+  await page.locator('nb-card')
+    .locator('nb-radio')
+    .locator(':text-is("Option 2")')
+    .click()
 
-  await page.locator('nb-card').getByRole('button', {name: 'Sign In'}).first().click()
+  await page.locator('nb-card')
+    .getByRole('button', { name: 'Sign In' })
+    .first()
+    .click()
 
-  await page.locator('nb-card').nth(3).getByRole('button').click()
+  await page.locator('nb-card')
+    .nth(3)
+    .getByRole('button')
+    .click()
+})
+
+test('locating parent elements', async ({ page }) => {
+
+  await page.locator('nb-card', { hasText: 'Using the Grid' })
+    .getByRole('textbox', { name: "Email" })
+    .click()
+  await page.locator('nb-card', { has: page.locator('#inputEmail1') })
+    .getByRole('textbox', { name: "Email" })
+    .click()
+
+  await page.locator('nb-card')
+    .filter({ hasText: 'Basic form' })
+    .getByRole('textbox', { name: "Email" })
+    .click()
+  await page.locator('nb-card')
+    .filter({ has: page.locator('.status-danger') })
+    .getByRole('textbox', { name: 'Email' })
+    .click()
+
+  await page.locator('nb-card')
+    .filter({ has: page.locator('nb-checkbox') })
+    .filter({ hasText: 'Sign in' })
+    .getByRole('textbox', { name: 'Email' })
+    .click()
+
+  await page.locator(':text-is("Using the Grid")')
+    .locator('..')
+    .getByRole('textbox', { name: 'Email' })
+    .click()
 })
