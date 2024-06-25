558156af65c4192f6ed6d6d0ad4689218799948f
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-25 21:33:28 +0300

    24. Locator Syntax Rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,31 +2,35 @@ import { test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/pages/iot-dashboard')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
 })
 
-test.describe('suite 1', () => {
+test('Locator syntax rules', async({page})=>{
+  //by Tag
+  await page.locator('input').first().click()
 
-  test.beforeEach(async ({ page }) => {
-    await page.getByText('Forms').click()
-  })
+  //by ID
+  await page.locator('#inputEmail1').click()
 
-  test('navigate to form layout page', async ({ page }) => {
-    await page.getByText('Form Layouts').click()
-  })
+  //by Class
+  page.locator('.shape-rectangle')
 
-  test('navigate to datepicker page', async ({ page }) => {
-    await page.getByText('Datepicker').click()
-  })
+  //by attribute
+  page.locator('[placeholder="Email1"]')
+
+  //by class value full
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+  //by combine different selectors
+  page.locator('input[placeholder="Email"][nbinput]')
+
+  //by xPath (not recommended)
+  page.locator('//*[@id="inputEmail1"]')
+
+  //by partial text match
+  page.locator(':text("Using")')
+
+  //by exact text match
+  page.locator(':text-is("Using the Grid")')
 })
-
-test.describe('suite 2', () => {
-
-  test.beforeEach(async ({ page }) => {
-    await page.getByText('Charts', {exact: true}).click()
-  })
-
-  test('navigate to Echarts page', async ({ page }) => {
-    await page.getByText('Echarts').click()
-  })
-})
-
