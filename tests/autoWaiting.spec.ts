581cd0cabf9a9f6fae48f9ba8d67b5803e53a120
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-26 20:56:16 +0300

    32. Timeouts

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from '@playwright/test'
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto('http://uitestingplayground.com/ajax')
   await page.getByText('Button Triggering AJAX Request').click()
+  testInfo.setTimeout(testInfo.timeout + 2000)
 })
 test('auto waiting', async ({ page }) => {
   const successButton = page.locator('.bg-success')
@@ -14,7 +15,7 @@ test('auto waiting', async ({ page }) => {
   //const text = await successButton.allTextContents()
   //expect(text).toContain('Data loaded with AJAX get request.')
 
-  //await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })
+  await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })
 })
 
 test('alternative waits', async ({ page }) => {
@@ -22,7 +23,7 @@ test('alternative waits', async ({ page }) => {
 
   //wait for element
   //await page.waitForSelector('.bg-success')
-  
+
   //wait for particular response
   //await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
 
@@ -34,4 +35,12 @@ test('alternative waits', async ({ page }) => {
 
 })
 
+test('timeouts', async ({ page }) => {
+  // test.setTimeout(10000)
+  test.slow()
+
+  const successButton = page.locator('.bg-success')
+  await successButton.click()
+
+})
 
