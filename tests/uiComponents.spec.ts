de20897ba29df7e865750208a6925d3b048c44d6
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-27 09:36:58 +0300

    35. Checkboxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -45,3 +45,17 @@ test.describe('Form Layouts page', () => {
 
 
 })
+
+test('checkboxes', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Toastr').click()
+
+  await page.getByRole('checkbox', { name: 'Hide on click' }).uncheck({ force: true })
+  await page.getByRole('checkbox', { name: 'Prevent arising of duplicate toast' }).check({ force: true })
+
+  const allBoxes = page.getByRole('checkbox')
+  for (const box of await allBoxes.all()) {
+    await box.uncheck({ force: true })
+    expect(await box.isChecked()).toBeFalsy()
+  }
+})
