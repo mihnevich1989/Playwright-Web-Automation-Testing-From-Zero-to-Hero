04afe5086dc325ac960343b622eaa672620036af
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-27 10:16:00 +0300

    37. Tooltips

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -92,3 +92,15 @@ test('lists and dropdowns', async ({ page }) => {
       await dropDownMenu.click()
   }
 })
+
+test('tooltips', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Tooltip').click()
+
+  const tooltipCard = page.locator('nb-card', { hasText: "Tooltip Placements" })
+  await tooltipCard.getByRole('button', { name: 'Top' }).hover()
+
+  page.getByRole('tooltip') //if we have a role tooltip created
+  const tooltip = await page.locator('nb-tooltip').textContent()
+  expect(tooltip).toEqual('This is a tooltip')
+})
