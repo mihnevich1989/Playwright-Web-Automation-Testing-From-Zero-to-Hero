6de92e323e448b2353714a3cf197fcf4ca751d55
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-28 21:22:30 +0300

    48. Locators in Page Objects (without recomendation by playwright)

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,46 +1,37 @@
-import { Locator, Page } from '@playwright/test'
+import { Page } from '@playwright/test'
 
 export class NavigationPage {
 
   readonly page: Page
-  readonly formLayoutsMenuItem: Locator
-  readonly datepickerMenuItem: Locator
-  readonly smartTableMenuItem: Locator
-  readonly toastrMenuItem: Locator
-  readonly tooltipMenuItem: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.formLayoutsMenuItem = page.getByText('Form Layouts')
-    this.datepickerMenuItem = page.getByText('Datepicker')
-    this.smartTableMenuItem = page.getByText('Smart Table')
-    this.toastrMenuItem = page.getByText('Toastr')
-    this.tooltipMenuItem = page.getByText('Tooltip')
   }
 
   async formLayotsPage() {
     await this.selectGroupMenuItem('Forms')
-    await this.formLayoutsMenuItem.click()
+    await this.page.getByText('Form Layouts').click()
   }
 
   async datepickerPage() {
     await this.selectGroupMenuItem('Forms')
-    await this.datepickerMenuItem.click()
+    await this.page.waitForTimeout(1000)
+    await this.page.getByText('Datepicker').click()
   }
 
   async smartTablePage() {
-    await this.selectGroupMenuItem('Tables & Data')
-    await this.smartTableMenuItem.click()
+    await this.selectGroupMenuItem('Tables & Data').click()
+    await this.page.getByText('Smart Table').click()
   }
 
   async toastrPage() {
-    await this.selectGroupMenuItem('Modal & Overlays')
-    await this.toastrMenuItem.click()
+    await this.selectGroupMenuItem('Modal & Overlays').click()
+    await this.page.getByText('Toastr').click()
   }
 
   async tooltipPage() {
-    await this.selectGroupMenuItem('Modal & Overlays')
-    await this.tooltipMenuItem.click()
+    await this.selectGroupMenuItem('Modal & Overlays').click()
+    await this.page.getByText('Tooltip').click()
   }
 
   private async selectGroupMenuItem(groupItemTitle: string) {
