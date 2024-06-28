b704f0f103a1abf979b0847246d3f8add0f95de9
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-28 10:07:06 +0300

    43. Sliders

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -187,3 +187,29 @@ test('date pickers', async ({ page }) => {
   await expect(calendarInputfield).toHaveValue(dateToAssert)
 })
 
+test('sliders', async ({ page }) => {
+  //update attribute
+  /* const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+  await tempGauge.evaluate(node => {
+    node.setAttribute('cx', '228')
+    node.setAttribute('cy', '228' )
+  })
+
+  await tempGauge.click() */
+
+  //mouse move
+  const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+  await tempBox.scrollIntoViewIfNeeded()
+
+  const box = await tempBox.boundingBox()
+  const x = box.x + box.width / 2
+  const y = box.y + box.height / 2
+
+  await page.mouse.move(x, y)
+  await page.mouse.down()
+  await page.mouse.move(x + 100, y)
+  await page.mouse.move(x + 100, y + 100)
+  await page.mouse.up()
+
+  await expect(tempBox).toContainText('30')
+})
