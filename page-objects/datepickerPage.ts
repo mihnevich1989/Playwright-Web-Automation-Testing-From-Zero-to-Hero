f82526f17c2c87b88a4fb8732a5de0931cee0fef
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-29 21:53:46 +0300

    50. Date Picker Page Object

---
+++ page-objects/datepickerPage.ts
@@ -0,0 +1,55 @@
+import { Page, expect } from '@playwright/test'
+
+export class DatepickerPage {
+
+  private readonly page: Page
+  constructor(page: Page) {
+    this.page = page
+  }
+
+  /**
+   * pick date in calendar and check it in filled field
+   * @param numberOfDaysFromToday choose a number how many days you want to add to today and start from this date (today + numberOfDays)
+   */
+  async selectCommonDatePickerDateFromToday(numberOfDaysFromToday: number) {
+    const calendarInputfield = this.page.getByPlaceholder('Form Picker')
+    await calendarInputfield.click()
+    const dateToAssert = await this.selectDateInTheCalendar(numberOfDaysFromToday)
+    await expect(calendarInputfield).toHaveValue(dateToAssert)
+  }
+
+  /**
+   * pick range between two dates in calendar and check it in filled field
+   * @param startDateFromToday start date from today
+   * @param endDateFromToday end  date from today
+   */
+  async selectDatepickerWithRangeFromToday(startDateFromToday: number, endDateFromToday: number) {
+    const calendarInputfield = this.page.getByPlaceholder('Range Picker')
+    await calendarInputfield.click()
+
+    const dateToAssertStart = await this.selectDateInTheCalendar(startDateFromToday)
+    const dateToAssertEnd = await this.selectDateInTheCalendar(endDateFromToday)
+    const dateToAssert = `${dateToAssertStart} - ${dateToAssertEnd}`
+    await expect(calendarInputfield).toHaveValue(dateToAssert)
+  }
+
+  private async selectDateInTheCalendar(numberOfDaysFromToday: number) {
+    let date = new Date()
+    date.setDate(date.getDate() + numberOfDaysFromToday)
+    const expectedDate = date.getDate().toString()
+    const expectedMonthShort = date.toLocaleString('En-US', { month: 'short' })
+    const expectedMonthLong = date.toLocaleString('En-US', { month: 'long' })
+    const expectedYear = date.getFullYear()
+    const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+
+    let calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `
+    while (!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+      await this.page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
+      calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
+    }
+
+    await this.page.locator('.day-cell.ng-star-inserted:not(.bounding-month)').getByText(expectedDate, { exact: true }).click()
+    return dateToAssert
+  }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage'
 import { FormLayoutPage } from '../page-objects/formLayoutsPage'
+import { DatepickerPage } from '../page-objects/datepickerPage'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/pages/iot-dashboard')
@@ -18,9 +19,12 @@ test('navigate to form page', async ({ page }) => {
 test('parametrized methods', async ({ page }) => {
   const navigateTo = new NavigationPage(page)
   const onFormLayoutsPage = new FormLayoutPage(page)
+  const onDatepickerPage = new DatepickerPage(page)
 
   await navigateTo.formLayotsPage()
   await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2')
   await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Wick', 'test@test.com', true)
+  await navigateTo.datepickerPage()
+  await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+  await onDatepickerPage.selectDatepickerWithRangeFromToday(0, 2)
 })
-
