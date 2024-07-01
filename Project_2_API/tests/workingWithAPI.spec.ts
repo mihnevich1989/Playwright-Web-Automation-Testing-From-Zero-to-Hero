c117a3ac939af0397b7f709ba005248f77553664
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-07-01 20:45:36 +0300

    58. Intercept Browser API Response

--- Project_2_API/tests/workingWithAPI.spec.ts
+++ Project_2_API/tests/workingWithAPI.spec.ts
@@ -72,3 +72,40 @@ test('delete article', async ({ page, request }) => {
 
   await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test title')
 })
+
+
+test('create article', async ({ page, request }) => {
+  await page.getByText('New Article').click()
+  await page.getByRole('textbox', { name: 'Article Title' }).fill("Playwright is awesome")
+  await page.getByRole('textbox', { name: 'What\'s this article about?' }).fill("About the playwright")
+  await page.getByRole('textbox', { name: 'Write your article (in markdown)' }).fill('We like to use playwright for automation')
+  await page.getByRole('button', { name: 'Publish Article' }).click()
+  const articleResponse = await page.waitForResponse('https://conduit-api.bondaracademy.com/api/articles/')
+  const articleResponseBody = await articleResponse.json()
+  const slugId = articleResponseBody.article.slug
+
+  await expect(page.locator('.article-page h1')).toContainText('Playwright is awesome')
+  await page.getByText('Home').click()
+  await page.getByText('Global Feed').click()
+  await expect(page.locator('app-article-list h1').first()).toContainText('Playwright is awesome')
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      "user":
+      {
+        "email": "dima.mihmih@mail.ru",
+        "password": "P@ssw0rd"
+      }
+    }
+  })
+
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+
+  const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  })
+  expect(deleteArticleResponse.status()).toEqual(204)
+})
