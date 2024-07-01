b6068702519ca6a2eace9695c730b4afa616a814
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-07-01 10:45:22 +0300

    56. Modify API Response

--- Project_2_API/tests/workingWithAPI.spec.ts
+++ Project_2_API/tests/workingWithAPI.spec.ts
@@ -3,15 +3,30 @@ import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({ page }) => {
 
-  await page.route('*/**/api/tags', async (route) => {
+  await page.route('*/**/api/tags', async route => {
     await route.fulfill({
       body: JSON.stringify(tags)
     })
   })
+
+  await page.route('*/**/api/articles?limit=10&offset=0', async route => {
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = 'This is a test title'
+    responseBody.articles[0].description = 'This is a test description'
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+
+  })
+
   await page.goto('https://conduit.bondaracademy.com/')
-  await page.waitForTimeout(500)
+  // await page.waitForTimeout(500)
 })
 
 test('has logo', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a test description')
 });
